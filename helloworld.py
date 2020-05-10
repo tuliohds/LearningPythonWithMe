137fffda275e0f5dc9b9a86782b58308df129bc6
Túlio Henrique <tuliohenriques@hotmail.com>
2020-05-09 22:35:35 -0300

    Learning Pyhton With Me
    In my first commit, I go from the classic hello world to the presentation of logical operators in Python.

---
+++ helloworld.py
@@ -0,0 +1,90 @@
+'''
+Hello people, Welcome to my lessons on Python.
+This job is make for help iniciant developers in Python.
+I hope you like it!
+
+My social medias
+Linkedin: linkedin.com/in/tuliohds
+Behance: behance.net/tuliohds
+Medium: medium.com/@tuliohenriquess
+GitHub: github.com/TulioHenrique
+
+Initialy you need make the donwload and the instalation of pyhton on your PC,
+after is just create one code in your favourite text editor, i use sublime.
+Write the code and press crtl+b for execute.
+'''
+
+# First Code, performing the classic Hello World
+print("Hello World")
+
+# Second Code is about mathematical expressions
+# Addition
+print("Addition: 10 + 2")
+print(10 + 3)
+# Subtraction
+print("Subtraction: 32 - 2")
+print(32 - 2)
+# Multiplication
+print("Multiplication: 10 x 10")
+print(10 * 10)
+# Division
+print("Division: 10 / 2")
+print(10 / 2)
+# Rest of division
+print("Rest of division: 10 / 3")
+print(10 % 3)
+
+# Now is time to use variables
+# Variables on Python are sensitive case, and the atribution operator is '='  
+msg = "Hello World using variables"
+print(msg)
+
+#Basic Data Types
+# Intengral numbers - Example: 47
+var1 = 1
+print(var1)
+
+# Float numbers - Example: 12.89
+var2 = 1.2
+print(var2)
+
+# Text (String) - Example: Hello World
+var3 = "I am a String"
+print(var3)
+
+# Boolean - Example: True or False
+var4 = True
+var5 = False
+print(var4)
+print(var5)
+
+#Basic Operators
+# Is used on operations in general
+print("+ | Addition")
+print("- | Subtraction")
+print("* | Multiplication")
+print("/ | Division")
+print("** | Exponentiation")
+print("% | Module")
+print("= | Atribution Operation")
+
+#Relationaly Operators
+# Is used in comparison operations
+print("== | Same")
+print("!= | Different")
+print("> | Larger")
+print("< | Minor")
+print(">= | Larger or Same")
+print("<= | Division")
+
+x = 2
+y = 3
+
+print(x > y) #False
+print(y > x) #True
+print(x == y) #False
+
+#Logical Operators
+print("AND | Two conditions must be True")
+print("OR | At least one condition must be True")
+print("NOT | Invert the value")
