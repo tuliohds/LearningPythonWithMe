61fb3941793579b5d058cda228080b76c484bc85
Túlio Henrique <tuliohenriques@hotmail.com>
2020-05-28 17:29:24 -0300

    Adding Lesson Dictionary
    Adding Lesson Dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -2,5 +2,23 @@
 
 #Dictionary
 
+# Dictionery are associations lists compose with:
+#	- a key
+# 	- a correspondent value
 
+my_dictionary = {"A":"Academic","B":"Ball","C":"Car"}
 
+print(my_dictionary["A"]) # Academic
+print(my_dictionary) # {'A': 'Academic', 'B': 'Ball', 'C': 'Car'}
+
+for key in my_dictionary:
+	print(key + ":" + my_dictionary[key])
+
+for i in my_dictionary.items():
+	print(i)
+
+for t in my_dictionary.values():
+	print(t)
+
+for v in my_dictionary.keys():
+	print(v)
