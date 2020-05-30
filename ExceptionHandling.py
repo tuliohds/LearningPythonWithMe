51abfdb14d482f3db62677fbc4c8f1385ef264ea
Túlio Henrique <tuliohenriques@hotmail.com>
2020-05-30 00:11:53 -0300

    Adding lessons of Random Numbers and Exception Handling
    Adding lessons of Random Numbers and Exception Handling

---
+++ ExceptionHandling.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+
+#ExceptionHandling
+
+# Now is time to making a example of exception handling
+a = 2
+b = 0
+
+try:
+	print(a/b)
+except:
+	print("Not is allowed division for 0")
---
+++ RandomNumbers.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+
+#Random Numbers
+
+# Python disponible models like this 
+import random
+
+# for use random numbers from one number to another
+number = random.randint(0,10)
+print(number)
+
+# for use random numbers with a list 
+list_number = [1,45,675,2] 
+number = random.choice(list_number)
+print(number)
